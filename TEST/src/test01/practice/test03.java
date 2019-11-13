a37d95af4a0130ebed510103357e369c9e54e7a7
JWH <JWH@DESKTOP-R4QBKAD>
2019-11-13 16:49:38 +0900

    practice

--- TEST/src/test01/practice/test03.java
+++ TEST/src/test01/practice/test03.java
@@ -1,4 +1,4 @@
-package test01;
+package test01.practice;
 
 import java.util.Scanner;
 import java.util.Random;
--- TEST/src/test01/practice/test05.java
+++ TEST/src/test01/practice/test05.java
@@ -1,4 +1,4 @@
-package test01;
+package test01.practice;
 
 import java.util.Scanner;
 
